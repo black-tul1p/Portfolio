8f524f76444633fab2370230be9e211455d9f5e8
Divay <divay7@gmail.com>
2022-10-16 11:11:05 -0500

    Moved files to components/ for clarity

--- strawberry/src/components/Profile.js
+++ strawberry/src/components/Profile.js
@@ -1,10 +1,10 @@
-import Me from "./resources/Professional.jpg";
+import Me from "../resources/Professional.jpg";
 
 export default function Profile() {
   return (
     <div>
       <div className="Profile-box">
-        <img src={Me} className="Profile-pic" />
+        <img src={Me} className="Profile-pic" alt="Divay Gupta" />
         <div className="Profile-text-box">
           <div id="My-info">
             <div className="Profile-text-top">Hi, I am</div>
